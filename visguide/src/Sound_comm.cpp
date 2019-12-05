3c23cf8643d95b7f843364ccc78405792efc23fa
Ayush Kumar <ka.kayushskhs24@gmail.com>
2019-12-05 16:47:48 -0500

    sound_comm.cpp

--- visguide/src/Sound_comm.cpp
+++ visguide/src/Sound_comm.cpp
@@ -4,35 +4,70 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "iostream"
 #include "unistd.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include <costmap_2d/costmap_2d_ros.h>
+#include <navfn/navfn_ros.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/OccupancyGrid.h>
+
 using namespace std;
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-void callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-	cout<<"checkpoint"<<endl;
-	
-	MoveBaseClient ac("move_base", true);
-	move_base_msgs::MoveBaseGoal goal;
-	goal.target_pose.pose = (*msg).pose;
-	goal.target_pose.header.frame_id = (*msg).header.frame_id;
-	
-	for (;;) {
-    usleep(5000000);
-    //cout<<"checkpoint2"<<endl;
-		goal.target_pose.header.stamp = ros::Time::now();
-		ac.sendGoal(goal);
-    //cout<<"checkpoint3"<<endl;
-  	//ac.waitForResult();
-    //cout<<"checkpoint3"<<endl;
-		cout<<"Publishing goal again"<<endl;
-	}
+nav_msgs:: OccupancyGrid grid_map;
+geometry_msgs::PoseStamped startPose;
+geometry_msgs::PoseStamped goalPose;
+vector<geometry_msgs::PoseStamped> plan;
+navfn::NavfnROS navfn_planner;
+
+void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+void callback(const nav_msgs::Odometry::ConstPtr & msg);
+
+
+void callback(const nav_msgs::Odometry::ConstPtr & msg) {
+	startPose.pose = (*msg).pose.pose;
 }
-int main(int argc, char** argv){
-  ros::init(argc, argv, "Nav_goals");
 
-  ros::NodeHandle n;
-  ros::Subscriber GoalSub = n.subscribe("/move_base_simple/goal", 100, callback);
-  ros::Rate loop_rate(10);
+void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+	goalPose.pose = (*msg).pose;
+
+	startPose.header.frame_id = "map";
+	startPose.header.stamp = ros::Time::now();
+
+	goalPose.header.frame_id = "map";
+    goalPose.header.stamp = ros::Time::now();
+
+	bool chk = navfn_planner.makePlan(startPose, goalPose, plan);
+	cout<<chk<<endl;
+	navfn_planner.publishPlan(plan, 0, 0, 255, 1);
+}
+
+void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
+	grid_map = (*msg);
+}
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "Sound_comm");
+
+	ros::NodeHandle n;
 	
-  ros::spin();
-  return 0;
+	tf2_ros::Buffer buffer;
+	tf2_ros::TransformListener tfl(buffer);
+	ros::Duration timeout(10.0);
+	costmap_2d::Costmap2DROS costmap("custom_map", buffer);
+	//costmap_2d::LayeredCostmap layers("map", false, true);
+
+	//costmap.getCostmap();
+    costmap.start();
+
+	
+    navfn_planner.initialize("my_navfn_planner", &costmap);
+
+	ros::Subscriber odomSub = n.subscribe("/custom_odom", 100, callback);
+	ros::Subscriber goalSub = n.subscribe("/move_base_simple/goal", 100, goalCallback);
+	//ros::Subscriber mapSub = n.subscribe("/rtabmap/grid_map", 100, mapCallback);
+
+	ros::spin();
+	return 0;
+
 }
